0752f96639731b06cee9d5a3ed28d22ce967695e
brandon-viterbo <brandonjviterbo@gmail.com>
2024-10-24 23:29:22 -0400

    Created weekly forecast row

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './styles.css';
 import { getThisWeeksForecast } from './getWeatherData';
-import { makeHourlyForecastRow } from './uiConstructors';
+import { makeHourlyForecastRow, makeWeeklyForecastRow } from './uiConstructors';
 
 //API key publically available, no consequence for exposing it.
 const myAPIkey = 'WXDR66S93RLSN689NRMZKADSK';
 
 const hourlyForecastRow = document.querySelector('.hourly > .forecast');
+const weeklyForecastRow = document.querySelector('.weekly');
+
 
 makeHourlyForecastRow(hourlyForecastRow);
+makeWeeklyForecastRow(weeklyForecastRow, 3);
--- src/uiConstructors.js
+++ src/uiConstructors.js
@@ -20,9 +20,62 @@ export function makeHourlyForecastRow(parentElement, intervalSize=3) {
     const data = hourCard.querySelector('.data');
     const hour = hourCard.querySelector('.hour');
 
-    data.textContent = '°15';
+    data.textContent = '15°';
     hour.textContent = `${i*intervalSize}:00`;
 
     parentElement.appendChild(hourCard);
+  }
+}
+
+function makeDayCard() {
+  const dayCard = document.createElement('div');
+  const day = document.createElement('div');
+  const icon = document.createElement('div');
+  const temp = document.createElement('div');
+  const high = document.createElement('div');
+  const low = document.createElement('div');
+
+  dayCard.classList.add('day-card');
+  day.classList.add('day');
+  icon.classList.add('icon');
+  temp.classList.add('temp');
+  high.classList.add('high');
+  low.classList.add('low');
+
+  temp.appendChild(high);
+  temp.appendChild(low);
+
+  dayCard.appendChild(day);
+  dayCard.appendChild(icon);
+  dayCard.appendChild(temp);
+
+  return dayCard;
+}
+
+function findDaysIndex(sum) {
+  //Sum is starting day index plus some int between 0-6.
+  if (sum >= 7) {
+    return sum - 7;
+  }
+
+  return sum;
+}
+
+export function makeWeeklyForecastRow(parentElement, startingDayIndex) {
+  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  for (let i = 0; i < 7; i++) {
+    const dayCard = makeDayCard();
+    const day = dayCard.querySelector('.day');
+    const icon = dayCard.querySelector('.icon');
+    const high = dayCard.querySelector('.high');
+    const low = dayCard.querySelector('.low');
+
+    day.textContent = days[findDaysIndex(i + startingDayIndex)];
+    icon.textContent = '☀️';
+    high.textContent = '23°';
+    low.textContent = '0°';
+
+    parentElement.appendChild(dayCard);
   }
 }
