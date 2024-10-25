f06530849af09db09991c7cc11a539794cb1f3ef
brandon-viterbo <brandonjviterbo@gmail.com>
2024-10-24 20:28:46 -0400

    Created function getThisWeeksForcast(), and helpers

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -12,10 +12,56 @@ function makeRequestURL(apiKey, city, startDate, endDate, url) {
   return requestURL;
 }
 
-export async function getCityWeather(apiKey, city, startDate='', endDate='', url=baseURL) {
+async function getCityWeather(apiKey, city, startDate='', endDate='', url=baseURL) {
   const requestURL = makeRequestURL(apiKey, city, startDate, endDate, url);
   const response = await fetch(requestURL);
   const weatherData = await response.json();
 
-  console.log(weatherData);
+  return weatherData;
 }
+
+function getHourlyForecastWithinDay(dailyWeatherData) {
+  const hours = dailyWeatherData.hours;
+  const cleanedHourlyData = hours.map((hr) => {
+    return {
+      datetime: hr.datetime,
+      feelslike: hr.feelslike,
+      humidity: hr.humidity,
+      precipprob: hr.precipprob,
+      preciptype: hr.preciptype,
+      temp: hr.temp,
+      winddir: hr.winddir,
+      windspeed: hr.windspeed,
+    }
+  });
+
+  return cleanedHourlyData;
+}
+
+function getDaysForecast(weatherData, dayIndex) {
+  const day = weatherData.days[dayIndex];
+  const daysForecast = {
+    datetime: day.datetime,
+    conditions: day.conditions,
+    feelslike: day.feelslike,
+    hours: getHourlyForecastWithinDay(day),
+  }
+
+  return daysForecast;
+}
+
+export async function getThisWeeksForecast(apiKey, city) {
+  const weatherData = await getCityWeather(apiKey, city);
+  const days = weatherData.days;
+  const weeksForecast = [];
+  
+  for (let i = 0; i < 7; i++) {
+    const day = days[i];
+    const daysData = getDaysForecast(weatherData, i);
+
+    weeksForecast.push(daysData);
+  }
+
+  return weeksForecast;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import { getCityWeather } from './getWeatherData';
+import { getThisWeeksForecast } from './getWeatherData';
 
 //API key publically available, no consequence for exposing it.
 const myAPIkey = 'WXDR66S93RLSN689NRMZKADSK';
