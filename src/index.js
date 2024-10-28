6c796c53aac0b32199f97f4b732eb88d3639de91
brandon-viterbo <brandonjviterbo@gmail.com>
2024-10-27 22:01:35 -0400

    Enabled user to switch between metric and imperial units

--- src/index.js
+++ src/index.js
@@ -12,28 +12,32 @@ const dayCards = weeklyForecastRow.children;
 const currentConditionsDisplay = document.querySelector('.current-conditions');
 const conditionToggles = document.querySelector('.hourly > .toggle');
 const conditions = conditionToggles.children;
-const hoverColor = window.getComputedStyle(document.documentElement).getPropertyValue('--hover-color');
 const searchBar = document.querySelector('input');
 const cityDisplay = document.querySelector('.city');
 const unitToggle = document.querySelector('.temp > .toggle');
 const units = unitToggle.children;
+const windUnitDisplay = document.querySelector('.wind .unit');
 
 let weatherData = {};
 let currentConditionsData = {};
 let weeklyConditionsData = {};
 let dayPicked = 0;
 
-async function initializeDisplay(cityName, dayToShow=0, unitToUse=0, dataToShow=0) {
+async function initializeDisplay(cityName, dayToShow=0, selectedUnitIndex=0, dataToShow=0) {
+  const isMetric = (selectedUnitIndex === 0);
+  const selectedCond = conditions[dataToShow]; 
+  const hourlyDataName = selectedCond.classList[0];
+  
   cityDisplay.textContent = cityName;
-  weatherData = await getCityWeather(myAPIkey, cityName);
+  weatherData = await getCityWeather(myAPIkey, cityName, isMetric);
   currentConditionsData = getCurrentConditions(weatherData);
   weeklyConditionsData = getThisWeeksForecast(weatherData);
   dayPicked = dayToShow;
   updateCurrentConditions(currentConditionsDisplay, currentConditionsData);
-  updateHourly(hourlyForecastRow, currentConditionsDisplay, currentConditionsData, weeklyConditionsData)
+  updateHourly(hourlyForecastRow, currentConditionsDisplay, currentConditionsData, weeklyConditionsData, dayToShow, hourlyDataName);
   updateWeeklyForecast(weeklyForecastRow, weeklyConditionsData);
-  highlightToggle(conditionToggles, unitToUse);
-  highlightToggle(unitToggle, dataToShow);
+  highlightToggle(conditionToggles, dataToShow);
+  highlightToggle(unitToggle, selectedUnitIndex);
 }
 
 makeHourlyForecastRow(hourlyForecastRow);
@@ -55,18 +59,26 @@ for (let i = 0; i < dayCards.length; i++) {
 
 for (let i = 0; i < conditions.length; i++) {
   const condition = conditions[i];
-
+  
   condition.addEventListener('click', (e) => {
     highlightToggle(conditionToggles, i);
     updateHourly(hourlyForecastRow, currentConditionsDisplay, currentConditionsData, weeklyConditionsData, dayPicked, condition.classList[0]);
   });
 }
 
+const windUnits = ['km/h', 'mph'];
 for (let i = 0; i < units.length; i++) {
   const element = units[i];
   
   element.addEventListener('click', (e) => {
+    windUnitDisplay.textContent = windUnits[i];
     highlightToggle(unitToggle, i);
+    
+    const cityName = cityDisplay.textContent;
+    const selectedCondition = document.querySelector('.hourly .selected');
+    const conditionIndex = parseInt(selectedCondition.dataset.index);
+
+    initializeDisplay(cityName, dayPicked, i, conditionIndex);
   });
 }
 
@@ -76,4 +88,4 @@ searchBar.addEventListener('keyup', (e) => {
     initializeDisplay(cityName);
     searchBar.value = '';
   }
-})
+});
